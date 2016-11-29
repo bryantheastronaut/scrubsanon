3de1cc43730fa0d8117295f6639b9bdbc5c2836c
bryantheastronaut <bgilbraith12@gmail.com>
2016-11-28 22:37:17 -0800

    uploading

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('style', function() {
+  gulp.src('sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('sass/*.scss', ['style']);
+});
